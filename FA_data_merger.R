c6cf4c585dd406adb36d7d679030e9bfd47e0edd
Mike V <vlahm13@gmail.com>
2015-08-17 12:10:43 -0700

    bugfix to FA_data_merger, update to .gitignore

--- FA_data_merger.R
+++ FA_data_merger.R
@@ -1,4 +1,8 @@
-dirs<-c("1_mirror_fish","2_mirror_fish","3_C_caddis","4_C_caddis","6_milk_caddis","7_mirror_caddis","8_morg_caddis","9_morg_caddis","10_noname_caddis","11_y015_caddis","12_y025_caddis","13_Z_caddis","90_C_cal","92_O_cal","108_clear_caddis")
+dirs<-c("1_mirror_fish","2_mirror_fish","3_C_caddis","4_C_caddis","6_milk_caddis","7_mirror_caddis",
+        "8_morg_caddis","9_morg_caddis","10_noname_caddis","11_y015_caddis","12_y025_caddis",
+        "13_Z_caddis","90_C_cal","92_O_cal","108_clear_caddis", "94_Z_cal", "85_mirror_cal", 
+        "86_y025_cal", "87_y015_cal", "80_clear_chaob", "96_L_cal", "81_clear_clad", "82_clear_cal", 
+        "83_morg_cal", "84_morg_clad", "93_O_clad", "105_milk_cal", "106_milk_chaob", "97_L_clad", "91_C_clad", "95_Z_clad")
 
 FAmerger<-function(dirs.list)
 {
@@ -12,7 +16,7 @@ FAmerger<-function(dirs.list)
   #loop through directories and perform data filtering/extraction
   for(i in 1:length(dirs.list))
   {
-    data<-read.csv(paste("C:/Users/Mike/Desktop/Grad/Projects/Thesis/Seven Lakes Project 2014/Data/FA/GC readouts/New folder (2)/",dirs.list[i],"/REPORT01.CSV",sep=""))
+    data<-read.csv(paste("C:/Users/Mike/Desktop/Grad/Projects/Thesis/Seven Lakes Project 2014/Data/FA/GC readouts/consumers/",dirs.list[i],"/REPORT01.CSV",sep=""))
     data<-data[!is.na(data$X),]
     data<-merge(X,data, all.y=T)
     data<-data[,c(1,3,6)]
@@ -27,12 +31,13 @@ FAmerger<-function(dirs.list)
   FA.frequency<-vector(length=length(merged[,1]))
   for(i in 1:length(merged[,1]))
   {
-    FA.frequency[i]<-sum(sapply(na.omit(as.numeric(as.vector(merged[i,2:31]))),is.numeric))/2
+    FA.frequency[i]<-sum(sapply(na.omit(as.numeric(as.vector(merged[i,(2:length(merged[1,]))]))),is.numeric))/2
   }
   
   #bind vector to "merged"
-  merged[[32]]<-FA.frequency
-  colnames(merged)[32]<-"total_appearances"
+  newCol<-(length(merged[1,])+1)
+  merged[[newCol]]<-FA.frequency
+  colnames(merged)[newCol]<-"total_appearances"
   
   return(merged)
 }
